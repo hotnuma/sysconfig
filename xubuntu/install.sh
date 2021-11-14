f95b8a4424932d8df30320f37973efa6e0bdfd22
hotnuma <hotnuma@gmail.com>
2021-11-15 00:54:34 +0100

    modify global script

--- xubuntu/install.sh
+++ xubuntu/install.sh
@@ -27,9 +27,7 @@ fi
 
 # install / remove -----------------------------------------------------
 
-# dconf-editor 
-
-dest=/usr/bin/htop
+dest=/usr/bin/mpv
 if [[ ! -f $dest ]]; then
     echo "*** install softwares"
     
@@ -42,6 +40,7 @@ if [[ ! -f $dest ]]; then
     sudo apt -y install htop geany rofi gimp xfce4-taskmanager net-tools
     sudo apt -y install p7zip-full engrampa lm-sensors hardinfo gparted
 	sudo apt -y install mpv mkvtoolnix mkvtoolnix-gui mediainfo-gui
+    sudo apt -y install build-essential git ninja-build dos2unix
     sudo apt -y install --no-install-recommends smartmontools
 	
 	# uninstall
@@ -62,7 +61,6 @@ if [[ ! -f $dest ]]; then
     echo "*** install dev softwares"
     
 	# install dev
-    sudo apt -y install build-essential git ninja-build dos2unix
 	sudo apt -y install qtcreator qtchooser qtbase5-dev qt5-qmake qtbase5-dev-tools
 	sudo apt -y install libgtk-3-dev gtk-3-examples libmediainfo-dev libprocps-dev
 	
