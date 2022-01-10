e795a17efa125e1b29a6020124036e1f0bad9ab5
hotnuma <hotnuma@gmail.com>
2022-01-10 22:30:10 +0100

    lxde: add base section

--- lxde/install.sh
+++ lxde/install.sh
@@ -4,6 +4,37 @@ BASEDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 OUTFILE="$HOME/install.log"
 rm -f $OUTFILE
 
+BASE=0
+DEV=0
+
+while [[ $# > 0 ]]; do
+    key="$1"
+    case $key in
+        base)
+        BASE=1
+        shift
+        ;;
+        dev)
+        DEV=1
+        shift
+        ;;
+        *)
+        shift
+        ;;
+    esac
+done
+
+ALL=$(( $BASE + $DEV ))
+
+if [[ $ALL < 1 ]]; then
+    echo "This scrip is not meant to be run as is, it will setup" 2>&1 | tee -a $OUTFILE
+    echo "overclocking, remove some programs, install others," 2>&1 | tee -a $OUTFILE
+    echo "disable bluetooth, wifi, etc... so it needs to be" 2>&1 | tee -a $OUTFILE
+    echo "studied and tweaked to perticuliar needs." 2>&1 | tee -a $OUTFILE
+    echo "abort..." 2>&1 | tee -a $OUTFILE
+    exit 1
+fi
+
 # test if sudo is succesfull -------------------------------------------
 
 if [[ "$EUID" = 0 ]]; then
@@ -17,47 +48,49 @@ else
     fi
 fi
 
-# passwordless sudo -------------------------------------------------
+if [[ $BASE == 1 ]]; then
 
-dest=/etc/sudoers.d/custom
-if [[ ! -f $dest ]]; then
-    echo " *** edit /etc/sudoers" 2>&1 | tee -a $OUTFILE
-    sudo tee $dest > /dev/null << 'EOF'
+	# passwordless sudo -------------------------------------------------
+
+	dest=/etc/sudoers.d/custom
+	if [[ ! -f $dest ]]; then
+		echo " *** edit /etc/sudoers" 2>&1 | tee -a $OUTFILE
+		sudo tee $dest > /dev/null << 'EOF'
 hotnuma ALL=(ALL) NOPASSWD: ALL
 
 EOF
-fi
+	fi
 
-dest=/usr/bin/openbox
-if [[ ! -f $dest ]]; then
-    echo " *** install desktop" 2>&1 | tee -a $OUTFILE
-    sudo pacman -Syu 2>&1 | tee -a $OUTFILE
+	dest=/usr/bin/openbox
+	if [[ ! -f $dest ]]; then
+		echo " *** install desktop" 2>&1 | tee -a $OUTFILE
+		sudo pacman -Syu 2>&1 | tee -a $OUTFILE
 
-    paklist="xorg ttf-liberation mesa openbox lxsession-gtk3 lxpanel-gtk3 \
-    pcmanfm-gtk3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
-    lxde-common lxde-icon-theme lxlauncher-gtk3 lxinput-gtk3 lxhotkey-gtk3 \
-    lxrandr-gtk3 lxappearance-gtk3 lxappearance-obconf-gtk3 lxtask-gtk3"
-    sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
-    
-    paklist="thunar gvfs xfce4-terminal xfce4-taskmanager wget htop geany \
-    mpv firefox firefox-ublock-origin engrampa meson cmake base-devel"
-    sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
-    
-    sudo systemctl enable lightdm.service --force 2>&1 | tee -a $OUTFILE
-fi
+		paklist="xorg ttf-liberation mesa openbox lxsession-gtk3 lxpanel-gtk3 \
+		pcmanfm-gtk3 lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
+		lxde-common lxde-icon-theme lxlauncher-gtk3 lxinput-gtk3 lxhotkey-gtk3 \
+		lxrandr-gtk3 lxappearance-gtk3 lxappearance-obconf-gtk3 lxtask-gtk3"
+		sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
+		
+		paklist="thunar gvfs xfce4-terminal xfce4-taskmanager wget htop geany \
+		mpv firefox firefox-ublock-origin engrampa meson cmake base-devel"
+		sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
+		
+		sudo systemctl enable lightdm.service --force 2>&1 | tee -a $OUTFILE
+	fi
 
-dest=/usr/bin/git
-if [[ ! -f $dest ]]; then
-    echo " *** install git" 2>&1 | tee -a $OUTFILE
-    paklist="git"
-    sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
-fi
+	dest=/usr/bin/git
+	if [[ ! -f $dest ]]; then
+		echo " *** install git" 2>&1 | tee -a $OUTFILE
+		paklist="git"
+		sudo pacman -S $paklist 2>&1 | tee -a $OUTFILE
+	fi
 
-dest=/boot/config.txt
-if [[ ! -f $dest.bak ]]; then
-    echo " *** edit config.txt" 2>&1 | tee -a $OUTFILE
-    sudo cp $dest $dest.bak 2>&1 | tee -a $OUTFILE
-    sudo tee $dest > /dev/null << 'EOF'
+	dest=/boot/config.txt
+	if [[ ! -f $dest.bak ]]; then
+		echo " *** edit config.txt" 2>&1 | tee -a $OUTFILE
+		sudo cp $dest $dest.bak 2>&1 | tee -a $OUTFILE
+		sudo tee $dest > /dev/null << 'EOF'
 # See /boot/overlays/README for all available options
 
 initramfs initramfs-linux.img followkernel
@@ -85,20 +118,21 @@ dtoverlay=disable-wifi
 dtoverlay=disable-bt
 
 EOF
-fi
+	fi
 
-# /home settings -------------------------------------------------------
+	# /home settings -------------------------------------------------------
 
-dest=~/config
-if [[ ! -d $dest ]]; then
-    echo " *** config link" 2>&1 | tee -a $OUTFILE
-    ln -s ~/.config $dest 2>&1 | tee -a $OUTFILE
-fi
+	dest=~/config
+	if [[ ! -d $dest ]]; then
+		echo " *** config link" 2>&1 | tee -a $OUTFILE
+		ln -s ~/.config $dest 2>&1 | tee -a $OUTFILE
+	fi
 
-dest=~/.config/autostart
-if [[ ! -d $dest ]]; then
-    echo " *** create autostart directory" 2>&1 | tee -a $OUTFILE
-    mkdir -p $dest 2>&1 | tee -a $OUTFILE
+	dest=~/.config/autostart
+	if [[ ! -d $dest ]]; then
+		echo " *** create autostart directory" 2>&1 | tee -a $OUTFILE
+		mkdir -p $dest 2>&1 | tee -a $OUTFILE
+	fi
 fi
 
 
