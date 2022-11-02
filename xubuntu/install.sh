cd4fabcf7558cf98206cf01a624e13b0e0aee652
hotnuma <hotnuma@gmail.com>
2022-11-02 01:00:10 +0100

    remove tabs

--- xubuntu/install.sh
+++ xubuntu/install.sh
@@ -31,18 +31,18 @@ if [[ ! -f $dest ]]; then
     echo "*** install softwares"
     
     # uninstall snaps
-	sudo rm -rf /var/cache/snapd/
-	sudo apt -y purge snapd gnome-software-plugin-snap
-	rm -rf ~/snap
-	
+    sudo rm -rf /var/cache/snapd/
+    sudo apt -y purge snapd gnome-software-plugin-snap
+    rm -rf ~/snap
+    
     # install base
     sudo apt -y install htop geany rofi gimp xfce4-taskmanager net-tools
     sudo apt -y install p7zip-full engrampa lm-sensors hardinfo gparted
     sudo apt -y install mpv mkvtoolnix mkvtoolnix-gui mediainfo-gui
     sudo apt -y install build-essential git meson ninja-build dos2unix
     sudo apt -y install --no-install-recommends smartmontools
-	
-	# uninstall
+    
+    # uninstall
     sudo apt -y purge xfce4-power-manager xfce4-screensaver tumbler
     sudo apt -y purge synaptic thunderbird fwupd thermald at-spi2-core
     
@@ -61,10 +61,10 @@ dest=/usr/bin/qtcreator
 if [[ ! -f $dest ]]; then
     echo "*** install dev softwares"
     
-	# install dev
-	sudo apt -y install qtcreator qtchooser qtbase5-dev qt5-qmake qtbase5-dev-tools
-	sudo apt -y install libgtk-3-dev gtk-3-examples libmediainfo-dev libprocps-dev
-	
+    # install dev
+    sudo apt -y install qtcreator qtchooser qtbase5-dev qt5-qmake qtbase5-dev-tools
+    sudo apt -y install libgtk-3-dev gtk-3-examples libmediainfo-dev libprocps-dev
+    
 fi
 
 
