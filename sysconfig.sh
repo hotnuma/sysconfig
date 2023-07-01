f4921a36eefbb6d05481c52c01d417eef0c1a8eb
hotnuma <hotnuma@gmail.com>
2023-07-01 16:16:26 +0200

    cleanup

--- sysconfig.sh
+++ sysconfig.sh
@@ -99,12 +99,12 @@ build_src "sysquery" "/usr/local/bin/sysquery"
 build_src "systools" "/usr/local/bin/colortest"
 build_src "taskman" "/usr/local/bin/xfce4-taskmanager"
 
+#~ build_src "applist" "/usr/local/bin/applist"
 #~ build_src "prgen" "/usr/local/bin/prgen"
 #~ build_src "mpvcmd" "/usr/local/bin/mpvcmd"
 #~ build_src "fileman" "/usr/local/bin/fileman"
 #~ build_src "volman" "/usr/local/bin/volman"
 #~ build_src "powerctl" "/usr/local/bin/powerctl"
-#~ build_src "applist" "/usr/local/bin/applist"
 
 # pop dir ---------------------------------------------------------------------
 
