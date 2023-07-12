d4d0bc384fc48b4f3356d2d48d369764c23d07f0
hotnuma <hotnuma@gmail.com>
2023-07-12 21:39:58 +0200

    raspios: simplify upgrade script

--- other/upgrade.sh
+++ other/upgrade.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/bash
 
-#sudo apt-get update && sudo apt-get dist-upgrade
+BASEDIR="$(dirname -- "$(readlink -f -- "$0";)")"
+DEBDIR="$BASEDIR/../debian"
+CURRENTUSER="$USER"
+OUTFILE="$HOME/install.log"
 
-sudo sed -i -e 's/bullseye/bookworm/g' /etc/apt/sources.list
-sudo sed -i -e 's/bullseye/bookworm/g' /etc/apt/sources.list.d/raspi.list
-sudo apt update
-sudo apt -y full-upgrade
-sudo apt -y autoremove
-sudo apt -y clean
-sudo reboot
+echo "===============================================================================" | tee -a $OUTFILE
+echo " Bookworm Upgrade..." | tee -a $OUTFILE
+echo "===============================================================================" | tee -a $OUTFILE
+
+dest=/etc/apt/sources.list
+if [[ ! -f ${dest}.bak ]]; then
+    echo " *** upgrade" | tee -a $OUTFILE
+	sudo cp "$dest" ${dest}.bak | tee -a $OUTFILE
+	sudo sed -i -e 's/bullseye/bookworm/g' /etc/apt/sources.list | tee -a $OUTFILE
+	sudo apt update | tee -a $OUTFILE
+	sudo apt upgrade | tee -a $OUTFILE
+fi
+
+#sudo sed -i -e 's/bullseye/bookworm/g' /etc/apt/sources.list.d/raspi.list
+#~ sudo apt -y full-upgrade
+#~ sudo apt -y autoremove
+#~ sudo apt -y clean
+#~ sudo reboot
 
 # remove old config files after doing sanity checks
 #sudo apt purge ?config-files
