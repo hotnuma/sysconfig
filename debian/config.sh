0e1daba1697646dbaebb682964f7e4b98bc63a54
hotnuma <hotnuma@gmail.com>
2024-12-28 05:33:38 +0100

    install qmake6

--- debian/config.sh
+++ debian/config.sh
@@ -144,8 +144,9 @@ fi
 dest=/usr/bin/qtcreator
 if [[ ! -f "$dest" ]]; then
     echo "*** install dev softwares" | tee -a "$OUTFILE"
-    APPLIST="qtcreator"
+    APPLIST="qtcreator qmake6"
     # https://packages.debian.org/bookworm/qtcreator
+    # https://packages.debian.org/source/bookworm/qt6-base
     # APPLIST+=" qtchooser qtbase5-dev qtbase5-dev-tools qt5-qmake"
     sudo apt -y install $APPLIST 2>&1 | tee -a "$OUTFILE"
 fi
