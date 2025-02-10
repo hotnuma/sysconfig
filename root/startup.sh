c3851af83cb7f12f318e8860fe9969ebe3e3d173
hotnuma <hotnuma@gmail.com>
2025-02-10 09:10:21 +0100

    set wallpaper if a file exists in the home directory

--- root/startup.sh
+++ root/startup.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/usr/bin/bash
 
-if [ "$XDG_CURRENT_DESKTOP"=="XFCE" ]; then
-    hsetroot -solid '#5e5c64'
+if [[ $XDG_CURRENT_DESKTOP != "XFCE" ]]; then
+    exit 0
 fi
 
+if [[ ! -f "/usr/bin/hsetroot" ]]; then
+    exit 0
+fi
+
+dest="$HOME"/wallpaper
+if [[ -f $dest ]]; then
+    hsetroot -cover "$dest"
+    exit 0
+fi
+
+hsetroot -solid '#5e5c64'
 
