27998dc26d95329cfacc81044e9f1498fefe0455
hotnuma <hotnuma@gmail.com>
2025-03-11 11:32:13 +0100

    add wget

--- sysconfig.sh
+++ sysconfig.sh
@@ -228,7 +228,7 @@ if [[ ! -f "$dest" ]]; then
     echo "*** install base" | tee -a "$outfile"
     APPLIST="curl dmz-cursor-theme dos2unix elementary-xfce-icon-theme"
     APPLIST+=" fonts-dejavu hsetroot htop net-tools p7zip-full python3-pip"
-    APPLIST+=" rofi"
+    APPLIST+=" rofi wget"
     APPLIST+=" audacious ffmpeg mkvtoolnix-gui mediainfo-gui mpv"
     APPLIST+=" engrampa geany gimp xfce4-screenshooter xfce4-terminal"
     APPLIST+=" zathura"
