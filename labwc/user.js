4536129efb344849dde7f7aaae8eff4525df28a6
hotnuma <hotnuma@gmail.com>
2025-03-12 20:43:59 +0100

    add user.js

---
+++ labwc/user.js
@@ -0,0 +1,31 @@
+
+// Good
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.update.auto", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.download.alwaysOpenPanel", false);
+user_pref("browser.eme.ui.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.tabs.hoverPreview.showThumbnails", false);
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.tipShownCount.searchTip_onboard", 4);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("extensions.pocket.enabled", false);
+//user_pref("font.minimum-size.x-western", 14);
+//user_pref("gfx.x11.glx_sgi_video_sync", true);
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("media.autoplay.default", 5);
+user_pref("media.gmp-gmpopenh264.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("ui.prefersReducedMotion", 1);
+user_pref("widget.gtk.overlay-scrollbars.enabled", false);
+
+
