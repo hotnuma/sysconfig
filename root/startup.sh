6132615763a7a784f9fda32fde51c3bf1c1f96d7
hotnuma <hotnuma@gmail.com>
2025-03-12 18:49:47 +0100

    change wallpaper path

--- root/startup.sh
+++ root/startup.sh
@@ -4,7 +4,7 @@ test "$XDG_CURRENT_DESKTOP" == "XFCE" || exit 0
 test "$XDG_SESSION_TYPE" == "x11" || exit 0
 which hsetroot || exit 0
 
-dest="$HOME/.wallpaper"
+dest="$HOME/.config/wallpaper"
 if [[ -f $dest ]]; then
     hsetroot -cover "$dest"
     exit 0
