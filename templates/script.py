844820d7d20ccbe640236c96bafd536b8f829ec0
hotnuma <hotnuma@gmail.com>
2025-03-18 10:03:37 +0100

    py template

--- templates/script.py
+++ templates/script.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os, sys, subprocess
-
 appname = os.path.basename(__file__)
 
 def error_exit(msg):
@@ -15,4 +14,21 @@ def usage_exit():
     print("%s -opt2 bla" % appname)
     print("abort...")
     exit(1)
+
+args = sys.argv
+size = len(args)
+if size < 2: usage_exit()
+
+opt_val = ""
+i = 1
+while i < size:
+    if args[i] == "-opt":
+        i += 1
+        if i >= size: error_exit("missing param")
+        opt_val = args[i]
+    else:
+        error_exit("invalid param")
+    i += 1
+
+print(opt_val)
 
