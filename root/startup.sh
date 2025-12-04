2fa0786a78c55eecf9551e503f834bb66bd5d8af
hotnuma <hotnuma@gmail.com>
2025-12-04 03:16:52 +0100

    labwc : use startup.sh

--- root/startup.sh
+++ root/startup.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 
-test "$XDG_CURRENT_DESKTOP" == "XFCE" || exit 0
-test "$XDG_SESSION_TYPE" == "x11" || exit 0
-which hsetroot || exit 0
-
-dest="$HOME/.config/wallpaper"
-if [[ -f $dest ]]; then
-    hsetroot -cover "$dest"
+if [[ $XDG_SESSION_TYPE == "x11" ]]; then
+    dest="$HOME/.config/wallpaper"
+    if [[ -f $dest ]]; then
+        hsetroot -cover "$dest"
+    else
+        hsetroot -solid '#5e5c64'
+    fi
     exit 0
 fi
 
-hsetroot -solid '#5e5c64'
+if [[ $XDG_SESSION_DESKTOP == "labwc" ]]; then
+    echo performance | sudo tee /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+    sleep 2
+    sudo systemctl restart systemd-timesyncd.service
+    exit 0
+fi
 
