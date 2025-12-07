0e3f50046af4d6b4a5a5fa5d36f891b80f78f5f4
hotnuma <hotnuma@gmail.com>
2025-12-07 05:21:28 +0100

    add user.js

---
+++ home/user.js
@@ -0,0 +1,56 @@
+// 20251207
+
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.update.auto", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.download.alwaysOpenPanel", false);
+user_pref("browser.eme.ui.enabled", false);
+user_pref("browser.newtab.preload", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.tabs.groups.enabled", false);
+user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.tabs.hoverPreview.showThumbnails", false);
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.tipShownCount.searchTip_onboard", 4);
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("dom.ipc.processPrelaunch.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("gfx.x11.glx_sgi_video_sync", false);
+user_pref("media.autoplay.blocking_policy", 2);
+user_pref("media.autoplay.default", 5);
+user_pref("media.gmp-gmpopenh264.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("ui.prefersReducedMotion", 1);
+user_pref("widget.gtk.overlay-scrollbars.enabled", false);
+
+// new settings
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.urlbar.scotchBonnet.enableOverride", false);
+user_pref("privacy.userContext.enabled", false);
+user_pref("privacy.userContext.ui.enabled", false);
+
+// https://www.reddit.com/user/001Guy001/
+user_pref("browser.ml.enable", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.chat.sidebar", false);
+user_pref("browser.ml.chat.menu", false);
+user_pref("browser.ml.chat.page", false);
+user_pref("extensions.ml.enabled", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("browser.tabs.groups.smart.enabled", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
+user_pref("pdfjs.enableAltTextModelDownload", false);
+user_pref("pdfjs.enableGuessAltText", false);
+
+// disabled
+//user_pref("privacy.resistFingerprinting", true);
+
